b1ba9ed6e59523efb966c494e4d2b6c579cf9c33
Monica McKeown <monimam5@gmail.com>
2026-03-26 18:43:43 -0600

    Update puzzles.js

--- puzzles.js
+++ puzzles.js
@@ -18,6 +18,22 @@
 
 const PUZZLES = [
   {
+    date: "2026-03-22",
+    name: "Pamplemousse",
+    aliases: ["Pamplemouse"],
+    grade: "V10",
+    location: "Brione, Switzerland",
+    photo: "images/pamplemousse.png"
+  },
+  {
+    date: "2026-03-23",
+    name: "Mountain Man",
+    aliases: [],
+    grade: "V9",
+    location: "Leavenworth, WA",
+    photo: "images/mountain_man.jpg"
+  },
+  {
     date: "2026-03-24",
     name: "Hell or High Water",
     aliases: ["Hell or Highwater"],
@@ -26,7 +42,7 @@ const PUZZLES = [
     photo: "images/hell_or_high_water.jpg"
   },
   {
-    date: "2026-03-26",
+    date: "2026-03-25",
     name: "Drawn and Quartered",
     aliases: ["Drawn", "Drawn n Quartered", "Drawn & Quartered"],
     grade: "V13",
@@ -34,23 +50,7 @@ const PUZZLES = [
     photo: "images/drawn_and_quartered.jpg"
   },
   {
-    date: "2026-03-27",
-    name: "Mountain Man",
-    aliases: [],
-    grade: "V9",
-    location: "Leavenworth, WA",
-    photo: "images/mountain_man.jpg"
-  },
-  {
-    date: "2026-03-28",
-    name: "Pamplemousse",
-    aliases: ["Pamplemouse"],
-    grade: "V10",
-    location: "Brione, Switzerland",
-    photo: "images/pamplemousse.png"
-  },
-  {
-    date: "2026-03-29",
+    date: "2026-03-26",
     name: "Pimpsqueak",
     aliases: ["Pimp Squeak", "Pimpsqueek"],
     grade: "V8",
@@ -58,11 +58,99 @@ const PUZZLES = [
     photo: "images/pimpsqueak.jpg"
   },
   {
+    date: "2026-03-27",
+    name: "The Finnish Line",
+    aliases: ["Finnish Line", "Finish Line", "The Finish Line"],
+    grade: "V15",
+    location: "Rocklands, South Africa",
+    photo: "images/finnish_line.jpg"
+  },
+  {
+    date: "2026-03-28",
+    name: "Loaded With Power",
+    aliases: ["Loaded"],
+    grade: "V10",
+    location: "Hueco, TX",
+    photo: "images/loaded_with_power.jpg"
+  },
+  {
+    date: "2026-03-29",
+    name: "Gription",
+    aliases: [],
+    grade: "V9",
+    location: "Moe's Valley, UT",
+    photo: "images/gription.jpg"
+  },
+  {
     date: "2026-03-30",
+    name: "Baba Hari Dass",
+    aliases: ["Baba", "Babaharidass", "Baba Haridass", "Baba Haridas", "Baba Hari Das"],
+    grade: "V7",
+    location: "Squamish, BC",
+    photo: "images/baba_hari_dass.jpg"
+  },
+  {
+    date: "2026-03-31",
+    name: "Takes a Village",
+    aliases: ["It Takes a Village"],
+    grade: "V13",
+    location: "Gold Bar, WA",
+    photo: "images/takes_a_village.jpg"
+  },
+  {
+    date: "2026-04-01",
     name: "The Sleeping Lady",
     aliases: ["Sleeping Lady"],
     grade: "V2",
     location: "Leavenworth, WA",
     photo: "images/sleeping_lady.jpg"
+  },
+  {
+    date: "2026-04-02",
+    name: "The Beaten Path",
+    aliases: ["Beaten Path"],
+    grade: "V10",
+    location: "Rocklands, South Africa",
+    photo: "images/beaten_path.jpg"
+  },
+  {
+    date: "2026-04-03",
+    name: "Midnight Lightning",
+    aliases: ["Midnite Lightning"],
+    grade: "V8",
+    location: "Yosemite, CA",
+    photo: "images/midnight_lightning.jpg"
+  },
+  {
+    date: "2026-04-04",
+    name: "The Hatchling RIP",
+    aliases: ["Hatchling"],
+    grade: "V11",
+    location: "Rocklands, South Africa",
+    photo: "images/hatchling.jpg"
+  },
+  {
+    date: "2026-04-05",
+    name: "Volition",
+    aliases: [],
+    grade: "V11",
+    location: "Gold Bar, WA",
+    photo: "images/volition.jpg"
+  },
+  {
+    date: "2026-04-06",
+    name: "Jabberwocky",
+    aliases: ["Jabberwock Arete", "Jabberwock"],
+    grade: "V4",
+    location: "Moe's Valley, UT",
+    photo: "images/jabberwocky.jpg"
+  },
+  {
+    date: "2026-04-07",
+    name: "The Rhino",
+    aliases: ["Rhino"],
+    grade: "V8",
+    location: "Rocklands, South Africa",
+    photo: "images/rhino.jpg"
   }
 ];
